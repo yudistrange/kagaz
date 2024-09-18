cebb260eedf783a509afa09c652d9f257824c3dc
udit <yudistrange@gmail.com>
2024-09-18 13:47:22 -0400

    Make the default tag for blogs as 'posts'

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -8,7 +8,7 @@ export const blogSchema = z
     postSlug: z.string().optional(),
     featured: z.boolean().optional(),
     draft: z.boolean().optional(),
-    tags: z.array(z.string()).default(["others"]),
+    tags: z.array(z.string()).default(["posts"]),
     ogImage: z.string().optional(),
     description: z.string(),
     canonicalURL: z.string().optional(),
