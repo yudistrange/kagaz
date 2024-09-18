da8a6528cf5c7219ff899db74dacf0eac952ef3a
udit <yudistrange@gmail.com>
2024-09-18 13:45:41 -0400

    Ensure sort and uniq works across blog | til | books

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,12 +1,10 @@
 import type { CollectionEntry } from "astro:content";
 
-const getSortedPosts = (posts: CollectionEntry<"blog">[]) =>
-  posts
-    .filter(({ data }) => !data?.draft)
-    .sort(
-      (a, b) =>
-        Math.floor(new Date(b.data.pubDatetime).getTime() / 1000) -
-        Math.floor(new Date(a.data.pubDatetime).getTime() / 1000)
-    );
+const getSortedPosts = (posts: CollectionEntry<"blog" | "til" | "book">[]) =>
+  posts.sort(
+    (a, b) =>
+      Math.floor(new Date(b.data.pubDatetime).getTime() / 1000) -
+      Math.floor(new Date(a.data.pubDatetime).getTime() / 1000)
+  );
 
 export default getSortedPosts;
